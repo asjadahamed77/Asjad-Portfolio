da62129a64bc7dc0be80ef32bf9140844ed50f20
asjadahamed77 <ahamedasjad77@gmail.com>
2025-05-15 15:10:27 +0530

    Completed Navbar responsively and added vercel file for deploy to the vercel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,20 @@ import React, { useEffect, useState } from "react";
 import { IoSunnyOutline } from "react-icons/io5";
 import { RiMoonClearLine } from "react-icons/ri";
 import { HiOutlineComputerDesktop } from "react-icons/hi2";
+import { IoMdMenu } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 
 const Navbar = () => {
   const [theme, setTheme] = useState(
     () => localStorage.getItem("theme") || "system"
   );
-  const [resolvedTheme, setResolvedTheme] = useState("light"); // actual applied theme
+  const [resolvedTheme, setResolvedTheme] = useState("light");
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const handleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  }
 
   useEffect(() => {
     const applyTheme = (mode) => {
@@ -61,12 +68,13 @@ const Navbar = () => {
   };
 
   return (
-    <div className=" px-4 sm:px-12 lg:px-32">
+    <div className=" px-4 sm:px-8 xl:px-32 ">
       <div className="flex items-center justify-between py-4 sticky top-0 left-0 right-0">
         <h1 className="bg-clip-text text-transparent bg-gradient-to-b from-mainBlueLight to-secondBlueLight dark:from-slate-50 dark:to-slate-200 text-2xl font-bold cursor-pointer">
           AA.
         </h1>
-        <div className="text-mainBlueLight dark:text-slate-50 text-sm flex items-center space-x-8">
+        <div className=" text-mainBlueLight dark:text-slate-50 text-sm flex items-center gap-8">
+          <div className="hidden text-mainBlueLight dark:text-slate-50 text-sm sm:flex items-center space-x-8">
           <a href="#home" className="relative group inline-block"  >
             Home
             <span className="absolute right-0 -bottom-1 h-1 rounded-full w-0 bg-mainBlueLight dark:bg-slate-50 transition-all duration-300 group-hover:w-2/3"></span>
@@ -87,8 +95,10 @@ const Navbar = () => {
           Contact
             <span className="absolute right-0 -bottom-1 h-1 rounded-full w-0 bg-mainBlueLight dark:bg-slate-50 transition-all duration-300 group-hover:w-2/3"></span>
           </a>
+          </div>
           
-          <div
+         <div className="flex items-center gap-4">
+         <div
             className={`flex items-center  text-lg  rounded-full ${
               resolvedTheme === "dark"
                 ? "bg-white/5 border border-white/30 text-white"
@@ -130,10 +140,32 @@ const Navbar = () => {
             >
               <IoSunnyOutline />
             </p>
+         
           </div>
-          <a href="/public/Asjad-Ahamed-Resume.pdf" download className="font-medium bg-gradient-to-r  from-mainBlueLight to-secondBlueLight dark:from-slate-50 dark:to-slate-200 dark:text-mainBlueLight text-white px-4 py-2 rounded-full cursor-pointer hover:opacity-90">My resume</a>
+          <p onClick={handleMobileMenu} className="sm:hidden text-mainBlueLight dark:text-slate-50 text-2xl cursor-pointer">
+            <IoMdMenu  />
+          </p>
+         </div>
+          <a href="/public/Asjad-Ahamed-Resume.pdf" download className="hidden sm:block font-medium bg-gradient-to-r  from-mainBlueLight to-secondBlueLight dark:from-slate-50 dark:to-slate-200 dark:text-mainBlueLight text-white px-4 py-2 rounded-full cursor-pointer hover:opacity-90">My resume</a>
         </div> 
+       
       </div>
+      {/* Mobile Menu */}
+      <div className={`fixed top-0 bottom-0 right-0 left-0 z-10 x sm:hidden bg-white/40 dark:bg-mainBlueLight/40 dark:text-white backdrop-blur-sm duration-300 transition-transform ease-linear 
+  ${mobileMenuOpen ? "translate-x-0" : "translate-x-full"}`}>
+    <div className="flex justify-end p-4">
+        <IoClose onClick={handleMobileMenu} className="text-2xl" />
+    </div>
+    <div className="w-full flex flex-col items-center gap-4 mt-12">
+        <a href="#home" onClick={handleMobileMenu} className="font-semibold text-xl">Home</a>
+        <a href="#about" onClick={handleMobileMenu} className="font-semibold text-xl">About</a>
+        <a href="#projects" onClick={handleMobileMenu} className="font-semibold text-xl">Projects</a>
+        <a href="#skills" onClick={handleMobileMenu} className="font-semibold text-xl">Skills</a>
+        <a href="#contact" onClick={handleMobileMenu} className="font-semibold text-xl">Contact</a>
+        <a href="/public/Asjad-Ahamed-Resume.pdf" download className=" font-medium bg-gradient-to-r  from-mainBlueLight to-secondBlueLight dark:from-slate-50 dark:to-slate-200 dark:text-mainBlueLight text-white px-4 py-2 rounded-full cursor-pointer hover:opacity-90">My resume</a>
+    </div>
+</div>
+
     </div>
   );
 };
