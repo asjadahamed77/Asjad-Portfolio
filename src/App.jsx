4101bda46f2d812eb092e5d42c646820e842e78f
asjadahamed77 <ahamedasjad77@gmail.com>
2025-05-16 15:22:43 +0530

    Added Assets and data to the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 
 function App() {
 
@@ -16,6 +17,8 @@ function App() {
  <Header />
      
     <About />
+
+    <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div id='projects' className='py-28'>
+      <h1 className="relative w-fit bg-clip-text text-transparent bg-gradient-to-b from-mainBlueLight to-secondBlueLight dark:from-slate-50 dark:to-slate-200 text-4xl font-bold ">
+        Projects
+        <span className="absolute -bottom-1 left-0 h-1 rounded-full bg-secondBlueLight w-2/5"></span>
+      </h1>
+    </div>
+  )
+}
+
+export default Projects
